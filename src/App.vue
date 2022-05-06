8e1cc3de2bcd08cccd8b3b5748d63df3c89ec870
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-05-06 15:43:35 +0200

    Intestazione

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <header-component/>
+    <main-component/> 
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue'
+import MainComponent from './components/MainComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    MainComponent
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
